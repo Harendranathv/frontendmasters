6ca8ab840c364ccb911707586a905a36b57f71a8
tvglobo <jefferson.ribeiro.contato@gmail.com>
2019-01-28 13:41:50 -0200

    #

--- testing/my_test_framework/beerjs/a.js
+++ testing/my_test_framework/beerjs/a.js
@@ -1,8 +1,22 @@
-function download() {
-  openTab($('.vjs-tech').getAttribute('src'));
+const navbar = document.querySelector('.FMPlayerNavright');
 
-  function openTab(url) {
-    var win = window.open(url, 'title', 'width=250 height=250');
-    win.focus();
-  }
+navbar.innerHTML = `
+<div class="FMPlayerNavButton FMPlayerComponent">
+<a href="#" class="button get-video">GET VIDEO</a>
+</div>
+`;
+
+const getVideoButton = document.querySelector('.get-video');
+
+getVideoButton.addEventListener('click', openTab);
+
+function openTab(e) {
+  e.preventDefault();
+  const video = document.querySelector('.vjs-tech');
+  const url = video.getAttribute('src');
+
+  video.pause();
+
+  var win = window.open(url, 'title', 'width=250 height=250');
+  win.focus();
 }
